a2a06acfcf014e7e5d8e01d38701e321d260ceb1
Benjamin <b.symanzik@googlemail.com>
2021-12-11 02:45:56 +0100

    ~change added new functions

--- calculator.js
+++ calculator.js
@@ -7,10 +7,36 @@ class Calculator {
 
     updateDisplay() {
         this.currentOperand.innerText = this.currentOperandInDisplay;
+        this.previousOperand.innerText = this.previousOperandInDisplay;
     }
 
     selectedOperation(operation) {
+        if(this.currentOperandInDisplay === '') return;
+        if(this.previousOperandInDisplay !== '') {
+            this.compute();
+        }
+        this.operation = operation;
+        this.previousOperandInDisplay = this.currentOperandInDisplay;
+        this.currentOperandInDisplay = '';
+    }
 
+    compute() {
+        let computation;
+        const previous = parseFloat(this.previousOperandInDisplay);
+        const current = parseFloat(this.currentOperandInDisplay);
+
+        if(isNaN(previous) || isNaN(current)) return;
+        switch(this.operation) {
+            case '+': computation = previous + current; break;
+            case '-': computation = previous - current; break;
+            case '*': computation = previous + current; break;
+            case '÷': computation = previous / current; break;
+            default: return;
+        }
+
+        this.currentOperandInDisplay = computation;
+        this.operation = undefined;
+        this.previousOperandInDisplay = '';
     }
 
     appendNumber(number) {
@@ -35,12 +61,32 @@ const numberButton = document.querySelectorAll('[data-number]');
 
 const calculator = new Calculator(previousOperand, currentOperand);
 
+clearButton.addEventListener('click', button => {
+    calculator.clear();
+    calculator.updateDisplay();
+})
+
 numberButton.forEach(button => {
     button.addEventListener('click', () => {
-        console.log(button);
         calculator.appendNumber(button.innerText);
         calculator.updateDisplay();
     })
 })
 
+equalButton.addEventListener('click', button => {
+    compute();
+})
 
+operationButton.forEach(button => {
+    button.addEventListener('keyup', (e) =>{
+        if(e.keyCode === 13){
+            calculator.selectedOperation(button.innerText);
+            calculator.updateDisplay();
+        }
+    })
+
+    button.addEventListener('click', () =>{
+        calculator.selectedOperation(button.innerText);
+        calculator.updateDisplay();
+    })
+})
